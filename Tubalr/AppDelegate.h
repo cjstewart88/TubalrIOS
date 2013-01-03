850f7b5e3e69bc7bdfc9976ce9d3b697358e024a
Chad Zeluff <czeluff@gmail.com>
2013-01-02 18:16:23 -0700

    initial project

---
+++ Tubalr/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tubalr
+//
+//  Created by Chad Zeluff on 1/2/13.
+//  Copyright (c) 2013 Chad Zeluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tubalr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Tubalr
+//
+//  Created by Chad Zeluff on 1/2/13.
+//  Copyright (c) 2013 Chad Zeluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
