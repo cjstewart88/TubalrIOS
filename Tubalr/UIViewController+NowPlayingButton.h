4162c6237ac0eaa15b5edb7f095f2e1c1d84b511
kwbock <kyle.bock@oracle.com>
2013-04-18 12:05:43 -0400

    adding now playing button and now playing category for the view controller

---
+++ Tubalr/UIViewController+NowPlayingButton.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+NowPlayingButton.h
+//  Tubalr
+//
+//  Created by Kyle Bock on 4/17/13.
+//  Copyright (c) 2013 Chad Zeluff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (NowPlayingButton)
+
+@end
---
+++ Tubalr/UIViewController+NowPlayingButton.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+NowPlayingButton.m
+//  Tubalr
+//
+//  Created by Kyle Bock on 4/17/13.
+//  Copyright (c) 2013 Chad Zeluff. All rights reserved.
+//
+
+#import "UIViewController+NowPlayingButton.h"
+
+@implementation UIViewController (NowPlayingButton)
+
+@end
